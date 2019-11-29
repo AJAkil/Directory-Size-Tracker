aef1d28a85eb6cb860a5eef2b6ef054057d0cfae
Akil <cyclosis47@gmail.com>
2019-11-29 14:36:59 +0600

    fixed bug of recursive and total directory size

--- FIle_size_printer.py
+++ FIle_size_printer.py
@@ -1,6 +1,6 @@
 import os
 
-path = os.getcwd()
+path = 'C:\Users\Asus\Music\\tester'
 
 
 
@@ -8,28 +8,36 @@ path = os.getcwd()
 def disk_usage(dir_path, size_list):
 
     '''we get the size of the current path'''
-    total_size = os.path.getsize(dir_path)
+    total_size = 0
+    #print('total_size at the beginning = '+ str(total_size))
     
     '''we check whether the current path is an directory, if yes, we recursively proceed ahead'''
     if(os.path.isdir(dir_path)):
-        
+        #print('entered for the path = ' + dir_path)
         for file_stuffs in os.listdir(dir_path):
 
             child_path = os.path.join(dir_path, file_stuffs)
-            total_size += os.path.getsize(child_path)
-            #print(total_size)
             child_size = os.path.getsize(child_path)
+
+            #print('path = {} child size = {}'.format(child_path, child_size))
+            
 
             if(child_size != 0L):
                 print('{} ---> {}'.format(child_path, child_size))
-                size_list.append(os.path.getsize(child_path))
+                size_list.append(child_size)
 
+            #print('state before entering recursion '+ child_path)
+            #print('before == ' + str(total_size))
             disk_usage(os.path.join(dir_path, file_stuffs), size_list)
+            total_size += size_list[len(size_list)-1]
+            #print('total Size afte adding up = '+str(total_size))
+            #print('after == ' + str(total_size))
 
         if(total_size != 0L):
+            print('entered here')
             print('{} ---> {}'.format(dir_path, total_size))
             size_list.append(total_size)
-        
+    
     return size_list
 
 
--- os_module_play.py
+++ os_module_play.py
@@ -4,11 +4,6 @@ path = os.getcwd()
 
 file_size = os.path.getsize(path+'\\b.txt')
 
-#print(file_size)
-
-#print(type(os.listdir(path)))
-
-
 def disk_usage(dir_path):
 
     '''we get the size of the current path'''
